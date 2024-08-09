b93f71c5364f112c45adde104ee8b3706b7be8f3
Rian1543 <165332241+Rian1543@users.noreply.github.com>
2024-08-08 21:07:36 -0300

    .

--- escola/views.py
+++ escola/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index (request):
-    return HttpResponse ('<html><body><h2>pagina escola<\h2><p>Olá,esta é a pagina escola</p></body></html>')
+        return HttpResponse ('<html><body><h2>Pagina Escola</h2><p>Olá, esta é a página escola</p></body></html>')
+
