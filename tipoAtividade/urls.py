2ecd63f49800e7b8f0c01cdf06ad508159910dd8
Rian1543 <165332241+Rian1543@users.noreply.github.com>
2024-08-15 21:24:27 -0300

    .

--- tipoAtividade/urls.py
+++ tipoAtividade/urls.py
@@ -4,6 +4,6 @@ from tipoAtividade import views
 urlpatterns = [
     
     #path('',views.inicio,name='index')
-    path('',views.cadastrar,name='cadastrar_tipoAtividades'),
+    path('',views.cadastrar,name='cadastrar_tipoAtividade'),
     path('listar/',views.listar,name='listar_tipoAtividade')
 ]
