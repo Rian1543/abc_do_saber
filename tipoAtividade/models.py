6526f64cd6a14f8cbe5ead756f078d7e41876080
Rian1543 <165332241+Rian1543@users.noreply.github.com>
2024-08-20 21:13:49 -0300

    .

--- tipoAtividade/models.py
+++ tipoAtividade/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class TipoAtividade(models.Model):
+    
+    codigo=models.IntegerField(primary_key=True,help_text='Codigo do tipo de atividade')
+    descricao=models.CharField(max_length=70,null=False,help_text='Informe a descricao do tipo de atividade')
+    
+    def __str__(self):
+        return f'{self.codigo} {self.descricao}'
+    
